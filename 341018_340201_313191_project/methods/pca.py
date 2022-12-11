c56bc337e4003da76ddfe3bab2eed203f2a78319
etienneasln <93213224+etienneasln@users.noreply.github.com>
2022-12-11 18:16:04 +0100

    PCA+KNN première version

--- 341018_340201_313191_project/methods/pca.py
+++ 341018_340201_313191_project/methods/pca.py
@@ -23,11 +23,13 @@ class PCA(object):
             The PCA class should have a variable defining the number of dimensions (d).
             You can either pass this as an arg or a kwarg.
         """
-        ##
-        ###
-        #### YOUR CODE HERE! 
-        ###
-        ##
+        if "d" in kwargs:
+            self.d = kwargs["d"]
+        elif len(args) >0 :
+            self.d = args[0]
+        else:
+            self.d = 1
+
 
     def find_principal_components(self, training_data):
         """
@@ -41,14 +43,32 @@ class PCA(object):
             Returns:
                 exvar (float): explained variance
         """
+        # Compute the mean of data
+        #This vector is a vector of dimension D (mean of the row vectors of X)
+        self.mean = np.mean(training_data,axis=0)
+        # Center the data with the mean
+        #X of size NxD and mean of size Dx1, we use broadcasting to substract to each row the value of the mean 
+        X_tilde = training_data-self.mean
+        # Create the covariance matrix
+        # N is the number of samples
+        N = training_data.shape[0]
+        C = (1/N)*(X_tilde.T@X_tilde)
+        # Compute the eigenvectors and eigenvalues. Hint: use np.linalg.eigh
+        eigvals, eigvecs = np.linalg.eigh(C)
+        # Choose the top d eigenvalues and corresponding eigenvectors. Sort the eigenvalues( with corresponding eigenvectors )
+        # in decreasing order first.
+        eigvals = np.flip(eigvals)
+        eigvecs = np.flip(eigvecs,axis=1)
 
-        ##
-        ###
-        #### YOUR CODE HERE! 
-        ###
-        ##
-        exvar=float(0)
-        return exvar
+        # Create matrix W and the corresponding eigen values
+        self.W = eigvecs[:,:self.d]
+        eg = eigvals[:self.d]
+
+        
+        # Compute the explained variance
+        exvar = np.sum(eg)/np.sum(eigvals)
+
+        return exvar*100
 
     def reduce_dimension(self, data):
         """
@@ -60,12 +80,7 @@ class PCA(object):
             Returns:
                 data_reduced (float): reduced data of shape (N,d)
         """
-        ##
-        ###
-        #### YOUR CODE HERE! 
-        ###
-        ##
         
-        return data_reduced
+        return  self.W.T@(data-self.mean)
         
 
